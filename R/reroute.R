29347a7e442c733213e9d62c2c513910185d15f3
James Blair <james.m.blair09@gmail.com>
2021-01-23 10:57:07 -0700

    Modify reroute() to use the updated form of standardized_path()

--- R/reroute.R
+++ R/reroute.R
@@ -5,13 +5,7 @@ reroute <- function(req, res) {
       if (body$script == "return int(1)") {
         return(1L)
       }
-      # If the leading / is present, overwrite the path. Otherwise, add the
-      # leading /
-      if (grepl("^/", body$script)) {
-        req$PATH_INFO <- body$script
-      } else {
-        req$PATH_INFO <- paste0("/", body$script)
-      }
+      req$PATH_INFO <- standardize_path(body$script)
     }
   }
   plumber::forward()
