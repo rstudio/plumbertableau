e384a5a548654c81376b726bc59dd1f979a36acf
James Blair <james.m.blair09@gmail.com>
2021-01-25 16:49:30 -0700

    Remove standardize_path()

--- R/reroute.R
+++ R/reroute.R
@@ -2,10 +2,14 @@ reroute <- function(req, res) {
   if (req$PATH_INFO == "/evaluate") {
     body <- jsonlite::fromJSON(req$postBody)
     if ("script" %in% names(body)) {
+      # This satisfies a Tableau requirement
+      # See https://tableau.github.io/analytics-extensions-api/docs/ae_known_issues.html
       if (body$script == "return int(1)") {
         return(1L)
       }
-      req$PATH_INFO <- standardize_path(body$script)
+      new_path <- body$script
+      if (!startsWith(new_path, "/")) new_path <- paste0("/", new_path)
+      req$PATH_INFO <- new_path
     }
   }
   plumber::forward()
