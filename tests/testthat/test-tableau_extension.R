b4e2903f3d93f410310739846837835ab36b8045
James Blair <james.m.blair09@gmail.com>
2021-02-12 16:17:36 -0700

    Add test for OpenAPI specification changes

--- tests/testthat/test-tableau_extension.R
+++ tests/testthat/test-tableau_extension.R
@@ -1,6 +1,6 @@
-test_that("stringutils example works", {
+pr_path <- system.file("examples/stringutils/plumber.R", package = "plumbertableau")
 
-  pr_path <- system.file("examples/stringutils/plumber.R", package = "plumbertableau")
+test_that("stringutils example works", {
 
   expect_identical(
     tableau_invoke(pr_path, "/stringutils/capitalize", "hello"),
@@ -16,4 +16,14 @@ test_that("stringutils example works", {
     tableau_invoke(pr_path, "/stringutils/concat?sep=-", letters, LETTERS),
     paste0(letters, "-", LETTERS)
   )
+})
+
+test_that("OpenAPI specification works", {
+  pr <- plumber::plumb(pr_path)
+  spec <- pr$getApiSpec()
+  for (path in spec$paths) {
+    if (!is.null(path$post)) {
+      expect_equal(path$post$requestBody$description, "Tableau Request")
+    }
+  }
 })
