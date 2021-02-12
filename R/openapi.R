1a862fc00b6379d87775e19754bf79fb2f608916
James Blair <james.m.blair09@gmail.com>
2021-02-12 16:12:11 -0700

    Move `tableau_spec` from list to YAML file; Modify the OpenAPI entry for each POST endpoint to use the `tableau_spec` to identify the requestBody

--- R/openapi.R
+++ R/openapi.R
@@ -1,42 +1,20 @@
+# Tableau POST spec
+tableau_spec <- yaml::read_yaml(system.file("openapi/tableau-evaluate-spec.yaml",
+                                            package = "plumbertableau"))
+
+
 #* Generate an OpenAPI file that matches Tableau's request specification
 tableau_openapi <- function(spec) {
+  for (i in 1:length(spec$paths)) {
+    if (!is.null(spec$paths[[i]][["post"]])) {
+      # For every post request, attach a description of the Tableau requestBody
+      # TODO: Is there a way to only identify post endpoints that are Tableau
+      # specific?
+      spec$paths[[i]][["post"]][["requestBody"]] <- tableau_spec
+    }
+  }
 
+  spec
 }
 
 
-#* Tableau POST spec
-tableau_spec <- list(
-  description = "Tableau Request",
-  required = TRUE,
-  content = list(
-    `application/json` = list(
-      schema = list(
-        type = "object",
-        required = c("script", "data"),
-        properties = list(
-          script = list(
-            type = "string",
-            title = "Path to desired endpoint",
-            example = "/foo"
-          ),
-          data = list(
-            type = "object",
-            required = "_arg1",
-            properties = list(
-              `_arg1` = list(
-                type = "array",
-                title = "First object passed from Tableau",
-                example = c(1)
-              ),
-              `_argN` = list(
-                type = "array",
-                title = "Other optional objects passed from Tableau",
-                example = c("a")
-              )
-            )
-          )
-        )
-      )
-    )
-  )
-)
