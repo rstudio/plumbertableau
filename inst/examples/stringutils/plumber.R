d0208ea4be0850dd5606e2e231e1dd55b7126c97
Joe Cheng <joe@rstudio.com>
2021-05-05 18:29:25 -0700

    Convert example to use comments

--- inst/examples/stringutils/plumber.R
+++ inst/examples/stringutils/plumber.R
@@ -4,22 +4,9 @@ library(plumbertableau)
 #* @apiTitle String utilities
 #* @apiDescription Simple functions for mutating strings
 
-#* Capitalize incoming text
-#* @post /capitalize
-#* @parser json
-tableau_handler(
-  args = list(
-    str_value = "character"
-  ),
-  return = "character",
-  function(req, res) {
-    toupper(str_value)
-  }
-)
-
 #* Lowercase incoming text
 #* @param unicode:boolean Whether unicode logic should be used
-#* @tab.arg str_value:[character]? Strings to be converted to lowercase
+#* @tab.arg str_value:[character] Strings to be converted to lowercase
 #* @tab.return [character] A lowercase string
 #* @post /lowercase
 #* @parser json
@@ -30,17 +17,13 @@ function(req, res, unicode = FALSE) {
 #* Concatenate
 #* @post /concat
 #* @param sep:str Separator value to use
+#* @tab.arg arg1:[character] One or more string values
+#* @tab.arg arg2:[character] One or more string values to concatenate to arg1
+#* @tab.return [character] arg1 and arg2 concatenated together
 #* @parser json
-tableau_handler(
-  args = list(
-    arg1 = "character",
-    arg2 = "character"
-  ),
-  return = "character",
-  function(req, res, sep = " ") {
-    paste(arg1, arg2, sep = sep)
-  }
-)
+function(req, res, sep = " ") {
+  paste(arg1, arg2, sep = sep)
+}
 
 #* @plumber
 tableau_extension("stringutils")
