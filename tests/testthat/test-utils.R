f7a0fa2e6eebcde2b62b4521cd62492b302fa361
James Blair <james.m.blair09@gmail.com>
2021-05-12 15:38:00 -0600

    Update warning tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -8,5 +8,5 @@ test_that("check_connect() works", {
 test_that("Tableau warnings fire as expected", {
   w <- capture_warnings(plumber::plumb(system.file("examples/mounts/plumber.R", package = "plumbertableau")))
   expect_match(w[1], "^Tableau endpoints must accept POST requests. /bar does not respond to POST requests.$")
-  expect_match(w[2], "^Tableau submits JSON requests. /bar does not include a JSON parser.$")
+  expect_match(w[2], "^Route /barincludes a user specified parser. plumbertableau automatically sets the appropriate parser for Tableau requests. There is no need to use #* @parser or pr_set_parsers().$")
 })
