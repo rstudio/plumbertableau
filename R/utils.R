5b0d58bd2318b309c5352c689634d344dc0ca6cc
James Blair <james.m.blair09@gmail.com>
2021-05-12 22:05:29 -0600

    Revert "Remove check for RStudio Connect"
    This reverts commit 2c9d5855b4c2a2236b6170c70fdc1f7b8b322f47.

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,17 @@
+#' Perform a series of checks to determine if the current code is executing on
+#' RStudio Connect. This intentionally casts a wide net since there's no
+#' persistent and reliable way to check of RStudio Connect is executing the R
+#' session
+check_connect <- function() {
+  env_vars <- Sys.getenv()
+  "RSTUDIO_CONNECT_HASTE" %in% names(env_vars) |
+    getwd() == "/opt/rstudio-connect/mnt/app" |
+    Sys.getenv("LOGNAME") == "rstudio-connect" |
+    Sys.getenv("R_CONFIG_ACTIVE") == "rsconnect" |
+    Sys.getenv("TMPDIR") == "/opt/rstudio-connect/mnt/tmp" |
+    grepl("^/opt/rstudio-connect/mnt/tmp", Sys.getenv("R_SESSION_TMPDIR"))
+}
+
 #' Check Plumber route for Tableau compliance
 #'
 #' @param route A plumber route
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,3 +1,10 @@
+test_that("check_connect() works", {
+  expect_false(check_connect())
+  Sys.setenv("R_CONFIG_ACTIVE" = "rsconnect")
+  expect_true(check_connect())
+  Sys.unsetenv("R_CONFIG_ACTIVE")
+})
+
 test_that("Tableau warnings fire as expected", {
   w <- capture_warnings(plumber::plumb(system.file("examples/mounts/plumber.R", package = "plumbertableau")))
   expect_match(w[1], "^Tableau endpoints must accept POST requests. /bar does not respond to POST requests.$")
