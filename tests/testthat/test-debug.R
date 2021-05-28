6738edd0e38866f78d5b59ab4cbb54a2dc1296b6
James Blair <james.m.blair09@gmail.com>
2021-05-28 14:26:59 -0600

    Remove devtools test dependency; Flesh out debug test regex

--- tests/testthat/test-debug.R
+++ tests/testthat/test-debug.R
@@ -2,7 +2,6 @@ pr_path <- system.file("examples/stringutils/plumber.R", package = "plumbertable
 
 test_that("debug logging works", {
   withr::local_envvar(c("DEBUGME" = "plumbertableau"))
-  devtools::reload()
   expect_output(tableau_invoke(pr_path, "/stringutils/capitalize", "hello"),
-                "^(plumbertableau)")
+                "^(plumbertableau) \\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\\]")
 })
