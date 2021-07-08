866386e217080d556e42c3be71209c1fb5bf140f
James Blair <james.m.blair09@gmail.com>
2021-07-08 16:00:06 -0600

    Add unit test for tableau arg and return details error

--- tests/testthat/test-tableau_handler.R
+++ tests/testthat/test-tableau_handler.R
@@ -1,3 +1,5 @@
+library(plumber)
+
 test_that("tableau_handler warns on missing function params", {
   args <- list(foo = arg_spec("character"), bar = arg_spec("character"))
 
@@ -12,4 +14,13 @@ test_that("tableau_handler warns on missing function params", {
   expect_warning(tableau_handler(args = args, return = return_spec(), func = function(req, res, bar) {}))
   expect_warning(tableau_handler(args = args, return = return_spec(), func = function(req, res, foo, bar) {}), NA)
   expect_warning(tableau_handler(args = args, return = return_spec(), func = function(req, res, ...) {}), NA)
+})
+
+test_that("Infer tableau handler throws an error when Tableau args and return types aren't provided", {
+  expect_error(
+    pr() %>%
+      pr_post("/foo", function() "foo") %>%
+      tableau_extension(),
+    regexp = "^Tableau argument and return data types"
+  )
 })
