48134fce29386a2590a421a03510fbbc1251e81a
James Blair <james.m.blair09@gmail.com>
2021-07-08 16:10:32 -0600

    Set default package options on package load

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,15 @@
 .onLoad <- function(libname, pkgname) {
+  # Debugging
   debugme::debugme()
+
+  # Options
+  op <- options()
+  op.plumbertableau <- list(
+    plumbertableau.warnings = TRUE
+  )
+
+  toset <- !(names(op.plumbertableau) %in% names(op))
+  if(any(toset)) options(op.plumbertableau[toset])
+
+  invisible()
 }
