d1ffe4a01506a794d8c41187ca34680fba054d26
Toph Allen <toph.allen@gmail.com>
2021-07-13 18:07:07 -0400

    Move *_message() funcs into openapi.R and combine_keys() to reroute.R

--- R/openapi.R
+++ R/openapi.R
@@ -105,3 +105,97 @@ json_type <- function(type) {
 }
 
 
+# Generate an informational message based on the execution context of the extension
+warning_message <- function() {
+  message_contents <- NULL
+  # RStudio Connect Details
+  # Provide messaging if RSC:
+  #  * Is a version that doesn't support Tableau Extensions
+  #  * Isn't configured to support Tableau Extensions
+  #  * Doesn't have Server.Address configured
+  # TODO: Replace this with proper logic once available
+
+  # Server.Address
+  connect_server <- Sys.getenv("CONNECT_SERVER")
+
+  # Does this installation support Tableau Extensions
+  connect_support <- Sys.getenv("RSC_TABLEAU")
+
+  # RStudio Connect version
+  connect_version <- Sys.getenv("RSC_VERSION")
+
+  if (connect_version != "1.9.0") {
+    message_contents <- paste(message_contents,
+                              "* **This version of RStudio Connect does not support Tableau Analytics Extension APIs. Please upgrade RStudio Connect to at least version 1.9.x**",
+                              sep = "\n")
+  }
+  if (!rlang::is_true(as.logical(connect_support))) {
+    message_contents <- paste(message_contents,
+                              "* This installation of RStudio Connect does not currently support Tableau Analytics Extension APIs.",
+                              sep = "\n")
+  }
+
+  if (connect_server == "") {
+    message_contents <- paste(message_contents,
+                              "* The `Server.Address` property isn't configured for this installation of RStudio Connect.",
+                              sep = "\n")
+  }
+
+  # Send warning message to the console if any of the above are TRUE
+  if (!rlang::is_null(message_contents)) {
+    message_contents <- paste(
+      message_contents,
+      "\n\n#### Please reach out to your RStudio Connect administrator",
+      sep = "\n"
+    )
+
+    rlang::warn(stringi::stri_replace_all(message_contents, regex = "\\* ?", replacement = ""), .frequency = "once", .frequency_id = "rsc_warning")
+  }
+
+  message_contents
+}
+
+
+info_message <- function() {
+  message_contents <- NULL
+  if (stringi::stri_detect(Sys.getenv("DEBUGME"), fixed = "plumbertableau")) {
+    message_contents <- paste(message_contents,
+                              "* Debugging is **on**.
+  * To disable it please remove the `DEBUGME` environment variable or set it to a value that does not include 'plumbertableau'.",
+  sep = "\n")
+  } else {
+    message_contents <- paste(message_contents,
+                              "* Debugging is **off**.
+  * To enable it please set the environment variable `DEBUGME` to include 'plumbertableau'.",
+  sep = "\n")
+  }
+
+  message_contents
+}
+
+
+# Generate message for extension setup
+extension_setup <- function() {
+  # TODO: Dynamically describe the actual values for Host and Port based on
+  # executing environment
+  "<details>
+  <summary>Tableau setup instructions</summary>
+
+#### Tableau Server / Tableau Online
+  1. Using an administrative account, login to Tableau Server
+  2. Navigate to Settings, then Extensions
+  3. At the bottom of the page, choose to *Enable analytics extensions for site*
+  4. Create a new connection and select the Connection Type of 'TapPy/Analytics Extension'
+  5. Select if you want to use SSL and enter the server Host and Port for your RStudio Connect server
+  6. If desired, select 'Sign in with a username and password'. The username is 'rstudio-connect' and the password is any valid API key from RStudio Connect
+  8. Save changes
+
+#### Tableau Desktop
+  1. Navigate to Help, Settings and Performance, Manage Analytics Extension Connection...
+  2. Select 'TabPy/External API'
+  3. Set Server and Port to the address and port of the server running the API
+  4. If desired, select 'Sign in with a username and password'. The username is 'rstudio-connect' and the password is any valid API key from RStudio Connect
+  5. Select whether to Require SSL
+  6. Save changes
+  </details>"
+}
--- R/reroute.R
+++ R/reroute.R
@@ -74,3 +74,108 @@ parseQS <- function(qs){
   # If duplicates, combine
   combine_keys(vals, type = "query")
 }
+
+
+#' @noRd
+#' @importFrom stats setNames
+combine_keys <- function(obj, type) {
+
+  keys <- names(obj)
+  unique_keys <- unique(keys)
+
+  # If a query string as the same amount of unique keys as keys,
+  # then return it as it
+  # (`"multi"` type objects MUST be processed, regardless if the unique key count is the same)
+  if (
+    length(unique_keys) == length(keys) &&
+    identical(type, "query")
+  ) {
+    return(obj)
+  }
+
+  vals <- unname(obj)
+
+  cleanup_item <- switch(
+    type,
+    "query" =
+      function(x) {
+        unname(unlist(x))
+      },
+    "multi" =
+      function(x) {
+        if (length(x) == 1) {
+          part <- x[[1]]
+          filename <- part$filename
+          parsed <- part$parsed
+
+          if (!is.null(filename)) {
+            # list(
+            #   "myfile.json" = list(
+            #     a = 1, b = 2
+            #   )
+            # )
+            return(
+              setNames(
+                list(parsed),
+                filename
+              )
+            )
+          }
+          # list(
+          #   a = 1, b = 2
+          # )
+          return(parsed)
+        }
+
+        # length is > 1
+
+        has_a_filename <- FALSE
+        filenames <- lapply(x, function(part) {
+          filename <- part$filename
+          if (is.null(filename)) return("")
+          has_a_filename <<- TRUE
+          filename
+        })
+
+        parsed_items <- lapply(unname(x), `[[`, "parsed")
+
+        if (!has_a_filename) {
+          # return as is
+          return(parsed_items)
+        }
+
+        return(setNames(parsed_items, filenames))
+      },
+    stop("unknown type: ", type)
+  )
+
+  # equivalent code output, `split` is much faster with larger objects
+  # Testing on personal machine had a breakpoint around 150 letters as query parameters
+  ## n <- 150
+  ## k <- sample(letters, n, replace = TRUE)
+  ## v <- as.list(sample(1L, n, replace = TRUE))
+  ## microbenchmark::microbenchmark(
+  ##   split = {
+  ##     lapply(split(v, k), function(x) unname(unlist(x)))
+  ##   },
+  ##   not_split = {
+  ##     lapply(unique(k), function(x) {
+  ##       unname(unlist(v[k == x]))
+  ##     })
+  ##   }
+  ## )
+  vals <-
+    if (length(unique_keys) > 150) {
+      lapply(split(vals, keys), function(items) {
+        cleanup_item(items)
+      })
+    } else {
+      # n < 150
+      lapply(unique_keys, function(key) {
+        cleanup_item(vals[keys == key])
+      })
+    }
+  names(vals) <- unique_keys
+
+  vals
+}
