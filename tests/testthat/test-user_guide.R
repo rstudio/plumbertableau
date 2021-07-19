63f30036e7825a4c458b93d947f573fa980b227d
Toph Allen <toph.allen@gmail.com>
2021-07-19 18:47:08 -0400

    change test name

--- tests/testthat/test-user_guide.R
+++ tests/testthat/test-user_guide.R
@@ -2,7 +2,7 @@
 # (https://testthat.r-lib.org/articles/snapshotting.html)
 # If it fails, but the user guide is displaying correctly, update it with 
 # > snapshot_accept("user_guide")
-test_that("user_guide()", {
+test_that("user_guide", {
   pr <- plumber::plumb(pr_path())
   expect_snapshot(cat(render_user_guide("", pr)))
 })
