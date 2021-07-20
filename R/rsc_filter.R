e20bbc3072943100cf6cd9f6008e21f079f7e730
James Blair <james.m.blair09@gmail.com>
2021-07-20 16:46:19 -0600

    Update `req$content_path` so the default value is NULL. This allows `rsc_filter()` to always be run, and simplifies the logic required when dealing with the path.

--- R/rsc_filter.R
+++ R/rsc_filter.R
@@ -1,8 +1,7 @@
 # A plumber filter for dealing with various details related to RStudio Connect
 rsc_filter <- function(req, res) {
   # Parse the endpoint path from header on RStudio Connect
-  # TODO: Improve logic for identifying content_path
-  # Look into RSTUDIO-CONNECT-APP-BASE-URL
+  req$content_path <- NULL
   if (!rlang::is_null(req$HTTP_RSTUDIO_CONNECT_APP_BASE_URL)) {
     base_url <- req$HTTP_RSTUDIO_CONNECT_APP_BASE_URL
     rsc_root <- Sys.getenv("CONNECT_SERVER")
