f637e965ba3fc84d1c9c9a5c7844044eeaeba040
Bill Sager <whs@sager.org>
2021-07-26 14:07:10 -0700

    paste to paste0

--- R/messages.R
+++ R/messages.R
@@ -17,19 +17,19 @@ warning_message <- function() {
     rsc_client <- connect()
     if (rsc_client$error_encountered) {
       message_contents <- paste(message_contents,
-        paste0("> **ERROR**: Unable to connect to RStudio Connect!", sep = "\n")
+        "> **ERROR**: Unable to connect to RStudio Connect!", sep = "\n")
       for (i in rsc_client$failure_messages) {
-        message_contents <- paste(message_contents, paste0("> {i}"), sep = "\n")
+        message_contents <- paste0(message_contents, "> {i}", sep = "\n")
       }
       continue_checking <- FALSE
     }
 
     if (continue_checking) {
       if (!rsc_client$validate() || rsc_client$error_encountered) {
-        message_contents <- paste(message_contents,
-          paste0("> **ERROR**: Unable to connect to RStudio Connect!", sep = "\n")
+        message_contents <- paste0(message_contents,
+          "> **ERROR**: Unable to connect to RStudio Connect!", sep = "\n")
         for (i in rsc_client$failure_messages) {
-          message_contents <- paste(message_contents, paste0("> {i}"), sep = "\n")
+          message_contents <- paste0(message_contents, "> {i}", sep = "\n")
         }
         continue_checking <- FALSE
       }
@@ -38,10 +38,10 @@ warning_message <- function() {
     if (continue_checking) {
       settings <- rsc_client$server_settings()
       if (is.null(settings) || rsc_client$error_encountered) {
-        message_contents <- paste(message_contents,
-          paste0("> **ERROR**: Unable to retrieve server settings from RStudio Connect!", sep = "\n")
+        message_contents <- paste0(message_contents,
+          "> **ERROR**: Unable to retrieve server settings from RStudio Connect!", sep = "\n")
         for (i in rsc_client$failure_messages) {
-          message_contents <- paste(message_contents, paste0("> {i}"), sep = "\n")
+          message_contents <- paste0(message_contents, "> {i}", sep = "\n")
         }
         continue_checking <- FALSE
       }
@@ -55,7 +55,7 @@ warning_message <- function() {
     connect_server <- Sys.getenv("CONNECT_SERVER")
 
     if (connect_server == "") {
-      message_contents <- paste(message_contents,
+      message_contents <- paste0(message_contents,
                                 "> **WARNING**: The `Server.Address` property is not set in RStudio Connect's configuration.\n",
                                 sep = "\n")
     }
@@ -67,20 +67,21 @@ warning_message <- function() {
     connect_version <- settings$version
 
     if (is.null(connect_support)) {
-      message_contents <- paste(message_contents,
+      message_contents <- paste0(message_contents,
                                 paste0("> **WARNING**: This version of RStudio Connect (",
                                       connect_version,
-                                      ") does not support Tableau Analytics Extension APIs. Please upgrade RStudio Connect.\n"),
+                                      ") does not support Tableau Analytics Extension APIs. Please upgrade RStudio Connect."
+                                ),
                                 sep = "\n")
     } else if (!connect_support) {
-      message_contents <- paste(message_contents,
-                                "> **WARNING**: Tableau Analytics Extension API support is currently disabled in RStudio Connect's configuration.\n",
+      message_contents <- paste0(message_contents,
+                                "> **WARNING**: Tableau Analytics Extension API support is currently disabled in RStudio Connect's configuration.",
                                 sep = "\n")
     }
 
     # Send warning message to the console if any of the above are TRUE
     if (!rlang::is_null(message_contents)) {
-      message_contents <- paste(
+      message_contents <- paste0(
         message_contents,
         "\n\n#### Please reach out to your RStudio Connect administrator to fix these issues.",
         sep = "\n"
@@ -99,13 +100,13 @@ warning_message <- function() {
 info_message <- function() {
   message_contents <- NULL
   if (stringi::stri_detect(Sys.getenv("DEBUGME"), fixed = "plumbertableau")) {
-    message_contents <- paste(
+    message_contents <- paste0(
       message_contents,
       "Verbose logging is on. To disable it please remove the `DEBUGME` environment variable or set it to a value that does not include 'plumbertableau'.",
       sep = "\n"
     )
   } else {
-    message_contents <- paste(
+    message_contents <- paste0(
       message_contents,
       "Verbose logging is off. To enable it please set the environment variable `DEBUGME` to include 'plumbertableau'.",
       sep = "\n"
@@ -118,13 +119,13 @@ info_message <- function() {
     settings <- rsc_client$server_settings()
     if (!rlang::is_null(settings$tableau_integration_logging)) {
       if (settings$tableau_integration_logging) {
-        message_contents <- paste(
+        message_contents <- paste0(
           message_contents,
           "RStudio Connect Tableau Logging is enabled. To disable it please set Tableau.Logging to false in the RStudio Connect configuration file.",
           sep = "\n"
         )
       } else {
-        message_contents <- paste(
+        message_contents <- paste0(
           message_contents,
           "RStudio Connect Tableau Logging is disabled To enable it please set Tableau.Logging to true in the RStudio Connect configuration file.",
           sep = "\n"
