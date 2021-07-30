32d8acb76cfc2021c3b575ff0cf64e4468834ab8
Bill Sager <whs@sager.org>
2021-07-30 11:00:14 -0700

    openAPI

--- R/openapi.R
+++ R/openapi.R
@@ -40,8 +40,8 @@ tableau_openapi <- function(pr) {
         "\n",
         "#### Use the following links to setup and use your Tableau Analytics Extension.",
         "\n",
-        "    * [Configure Tableau to access your extension](../setup)",
-        "    * [How to use your extension from Tableau](../)",
+        "*    [Configure Tableau to access your extension](../setup)",
+        "*     [How to use your extension from Tableau](../)",
         sep = "\n"
       )
     }
