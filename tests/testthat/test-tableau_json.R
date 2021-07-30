a9eaf445732c74d2b91e2c2ff0e55f4fc5f6a23c
James Blair <james.m.blair09@gmail.com>
2021-07-29 23:29:04 -0600

    Fix unit test to reference `mock_tableau_request()`

--- tests/testthat/test-tableau_json.R
+++ tests/testthat/test-tableau_json.R
@@ -1,4 +1,4 @@
-test_that("tableau_json() works", {
+test_that("mock_tableau_request() works", {
   json <- jsonlite::toJSON(list(
     script = jsonlite::unbox("/foo"),
     data = list(
@@ -7,12 +7,12 @@ test_that("tableau_json() works", {
     )
   ))
 
-  expect_identical(tableau_json("/foo", list(x = letters, y = 1:length(letters))),
+  expect_identical(mock_tableau_request("/foo", list(x = letters, y = 1:length(letters))),
                    json)
 
-  expect_error(tableau_json("/foo", list(x = 1:3, y = 1:5)),
+  expect_error(mock_tableau_request("/foo", list(x = 1:3, y = 1:5)),
                "^All entries in data must be of equal length.$")
 
-  expect_error(tableau_json("/foo", "this is not a list"),
+  expect_error(mock_tableau_request("/foo", "this is not a list"),
                "data must be a list or data.frame object")
 })
