449e6f831b86915e1e3a2d722e90cb43de26e201
Bill Sager <whs@sager.org>
2021-07-30 13:24:29 -0700

    test

--- R/user_guide.R
+++ R/user_guide.R
@@ -350,5 +350,8 @@ fragment.only = TRUE
 strip_md_links <- function(text) {
 
   ### Description
-  stringi::stri_replace_all(text, regex = "\n#### Use the following links.*", "")
+  desc <- stringi::stri_replace_all(text, regex = "\n### Description.*", "")
+  desc <- stringi::stri_replace_all(desc, regex = "\n* [Configure Tableau to access your extension](../setup)", "")
+  desc <- stringi::stri_replace_all(desc, regex = "\n* [How to use your extension from Tableau](../)", "")
+  desc
 }
