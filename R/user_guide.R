9b2b3e3f5c0d4d80153aa3819dbb041b583926b8
Bill Sager <whs@sager.org>
2021-07-30 14:39:04 -0700

    t

--- R/user_guide.R
+++ R/user_guide.R
@@ -327,7 +327,7 @@ render_setup_instructions <- function(path, pr) {
           tags$li("Navigate to Settings, then Extensions"),
           tags$li("Under the heading 'Analytics Extensions', select 'Enable analytics extension for site"),
           tags$li("Create a new connection and select the connection type of 'Analytics Extensions API'"),
-          tags$li("Select if you want to use SSL and enter the server Host (`{server_domain}`) and Port (`{server_port}`) for your RStudio Connect server"),
+          tags$li(glue::glue("Select if you want to use SSL and enter the server Host (`{server_domain}`) and Port (`{server_port}`) for your RStudio Connect server")),
           tags$li("Select 'Sign in with a username and password'. The username is 'rstudio-connect' and the password is any valid API key from RStudio Connect"),
           tags$li("Create / Save changes")
         )
