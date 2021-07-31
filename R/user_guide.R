55771ae936b889f266131c4b65a7b153b022c64e
Bill Sager <whs@sager.org>
2021-07-31 16:30:06 -0700

    test menu

--- R/user_guide.R
+++ R/user_guide.R
@@ -352,6 +352,7 @@ render_setup_instructions <- function(path, pr) {
       )
     ),
     tags$main(
+      htmltools::HTML(menu_html()),
       tags$h3(
         class="subtitle",
         "Configure Tableau to access your extension"
@@ -438,4 +439,97 @@ render_setup_instructions <- function(path, pr) {
 strip_md_links <- function(text) {
   i <- stringi::stri_locate_first(text, fixed = "#### Use the following links to setup and use your Tableau Analytics Extension.")
   substr(text, start=1, stop=i-1)
+}
+
+menu_html <- function() {
+'
+  <div class="area"></div>
+  <nav class="main-menu">
+    <ul>
+      <li>
+        <a href="http://justinfarrow.com">
+          <i class="fa fa-home fa-2x"></i>
+          <span class="nav-text">
+              Dashboard
+          </span>
+        </a>
+      </li>
+      <li class="has-subnav">
+        <a href="#">
+          <i class="fa fa-laptop fa-2x"></i>
+          <span class="nav-text">
+              Stars Components
+          </span>
+        </a>
+      </li>
+      <li class="has-subnav">
+        <a href="#">
+          <i class="fa fa-list fa-2x"></i>
+          <span class="nav-text">
+              Forms
+          </span>
+        </a>
+      </li>
+      <li class="has-subnav">
+        <a href="#">
+          <i class="fa fa-folder-open fa-2x"></i>
+          <span class="nav-text">
+              Pages
+          </span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fa fa-bar-chart-o fa-2x"></i>
+          <span class="nav-text">
+              Graphs and Statistics
+          </span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fa fa-font fa-2x"></i>
+          <span class="nav-text">
+              Quotes
+          </span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fa fa-table fa-2x"></i>
+          <span class="nav-text">
+              Tables
+          </span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fa fa-map-marker fa-2x"></i>
+          <span class="nav-text">
+              Maps
+          </span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fa fa-info fa-2x"></i>
+          <span class="nav-text">
+              Documentation
+          </span>
+        </a>
+      </li>
+    </ul>
+
+    <ul class="logout">
+      <li>
+        <a href="#">
+          <i class="fa fa-power-off fa-2x"></i>
+          <span class="nav-text">
+              Logout
+          </span>
+        </a>
+      </li>  
+    </ul>
+  </nav>
+  '
 }
