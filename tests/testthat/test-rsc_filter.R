36ebdd9ea57aa0bfe32a0f86bff8272c954c4e72
James Blair <james.m.blair09@gmail.com>
2021-08-02 21:02:43 -0600

    Update `test-rsc_filter.R`

--- tests/testthat/test-rsc_filter.R
+++ tests/testthat/test-rsc_filter.R
@@ -1,9 +1,11 @@
   req <- make_req(
     verb = "POST",
     path = "/loess",
-    postBody = encode_payload(script = "concat", letters, .toJSON_args = NULL, raw = FALSE)
+    postBody = encode_payload(script = "concat", letters, .toJSON_args = NULL, raw = FALSE),
+    HTTP_X_RS_CORRELATION_ID = 123456
   )
 
-test_that("rsc_filter() correctly rewrites requests", {
-  # TODO: Add test cases once rsc_filter() is finalized.
+test_that("rsc_filter() correctly modifies requests", {
+  rsc_filter(req)
+  expect_equal(req$HTTP_X_CORRELATION_ID, 123456)
 })
